c8c82d5102ba86f0abed57034dda77347d0f308a
Hansol Kim <kimhs@bien.ltd>
2025-11-28 16:06:10 +0900

    테트리스 블럭 움직이는 로직, 보드 안에서만 움직일 수 있게 구현

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,9 @@
 export const COLS = 10;
 export const ROWS = 20;
 export const BLOCKSIZE = 30;
+
+export const KEY = {
+  LEFT: 37,
+  RIGHT: 39,
+  DOWM: 40,
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { BLOCKSIZE, COLS, ROWS } from "./constant";
+import { BLOCKSIZE, COLS, KEY, ROWS } from "./constant";
 
 const canvas = document.querySelector("#board");
 const ctx = canvas.getContext("2d");
@@ -8,8 +8,14 @@ ctx.canvas.height = ROWS * BLOCKSIZE;
 
 ctx.scale(BLOCKSIZE, BLOCKSIZE);
 
+let block;
+
 const reset = () => {
   return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+};
+
+const isInsideBoard = (x, y) => {
+  return x >= 0 && x < COLS && y >= 0 && y < ROWS;
 };
 
 console.table(reset());
@@ -48,14 +54,50 @@ class Block {
       });
     });
   }
+
+  move(p) {
+    this.x = p.x;
+    this.y = p.y;
+  }
 }
 
 function play() {
   reset();
-  const block = new Block();
+  block = new Block();
   block.draw();
 }
 
 document.querySelector("#playButton").addEventListener("click", () => {
   play();
+});
+
+const moves = {
+  [KEY.LEFT]: (p) => ({ ...p, x: p.x - 1 }),
+  [KEY.RIGHT]: (p) => ({ ...p, x: p.x + 1 }),
+  [KEY.DOWM]: (p) => ({ ...p, y: p.y + 1 }),
+};
+
+document.addEventListener("keydown", (event) => {
+  if (block && moves[event.keyCode]) {
+    const isValidMove = (p) => {
+      return block.shape.every((row, dy) => {
+        return row.every((cell, dx) => {
+          if (cell === 0) return true;
+
+          const newX = p.x + dx;
+          const newY = p.y + dy;
+
+          if (!isInsideBoard(newX, newY)) return false;
+          return true;
+        });
+      });
+    };
+
+    const position = moves[event.keyCode]({ x: block.x, y: block.y });
+    if (isValidMove(position)) {
+      block.move(position);
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      block.draw();
+    }
+  }
 });
